8c818ff6fc6c4a6380fce2c80b48d933ff48cf9e
chad hinds <chadrickj8@gmail.com>
2023-10-13 11:54:52 -0400

    Remove boilerplate images, add new image

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,22 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
+import { Card, CardBody } from "@nextui-org/react";
+import Image from "next/image";
 
 export default function IndexPage() {
 	return (
 		<DefaultLayout>
 			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-
+				<Card>
+					<CardBody>
+						<div className="felx flex-col w-full">
+							<div className="flex justify-center">
+								<Image alt="CoopTing profile picture" src="/CoopTing.png" width={200} height={200} />
+							</div>
+						</div>
+					</CardBody>
+				</Card>
 			</section>
 		</DefaultLayout>
 	);
